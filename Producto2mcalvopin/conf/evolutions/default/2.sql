6bfe20177c234bfeebd1509dce83bb63ae98e80c
mcalvopiUTC <martha.calvopina1@utc.edu.ec>
2016-12-12 09:05:14 -0500

    mcalvopinUTC
    mcalvopinutc

---
+++ Producto2mcalvopin/conf/evolutions/default/2.sql
@@ -0,0 +1,28 @@
+# --- Sample dataset
+
+# --- !Ups
+
+insert into plato (id,name,costo,ingrediente,descripcion) values (  1,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  2,'Arroz con pollo',4,'Pollo','Comida tradicional');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  3,'Seco de chancho',5,'Chancho','Comida tradicional');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  4,'Seco de chivo',8,'Chivo','Comida tradicional');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  5,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  6,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  7,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  8,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  9,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  10,'Ensalada Verde',8,'Lechuga','Saludable');    
+insert into plato (id,name,costo,ingrediente,descripcion) values (  11,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  12,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  13,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  14,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  15,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  16,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  17,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  18,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  19,'Ensalada Verde',8,'Lechuga','Saludable');
+insert into plato (id,name,costo,ingrediente,descripcion) values (  20,'Ensalada Verde',8,'Lechuga','Saludable');
+# --- !Downs
+
+delete from plato;
+
